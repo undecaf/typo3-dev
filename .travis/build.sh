a9aded1b31614d1e47581d568d04ca526ca565ce
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2019-05-09 19:16:47 +0200

    Shorten tag only to MAJOR.MINOR

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,15 +2,15 @@
 
 # Usage: build.sh [OWNER/REPO] [TAG]
 
-expand_tag() {
-    test "$1" = "${1%.*}" && echo "$1" || echo "$(expand_tag ${1%.*}) $1"
+shorten_tag() {
+    test "$1" != "${1%.*}" && echo "$(shorten_tag ${1%.*}) $1"
 }
 
 OWNER_REPO=$1
 
 docker build --pull --cache-from $OWNER_REPO --tag $OWNER_REPO .
 
-for T in $(expand_tag $2) latest; do 
+for T in $(shorten_tag $2) latest; do 
     echo "*************** Tagging $OWNER_REPO as $OWNER_REPO:$T"
     docker tag $OWNER_REPO $OWNER_REPO:$T
 done
--- .travis/push.sh
+++ .travis/push.sh
@@ -2,13 +2,13 @@
 
 # Usage: push.sh [OWNER/REPO] [TAG]
 
-expand_tag() {
-    test "$1" = "${1%.*}" && echo "$1" || echo "$(expand_tag ${1%.*}) $1"
+shorten_tag() {
+    test "$1" != "${1%.*}" && echo "$(shorten_tag ${1%.*}) $1"
 }
 
 OWNER_REPO=$1
 
-for T in $(expand_tag $2) latest; do 
+for T in $(shorten_tag $2) latest; do 
     echo "*************** Pushing $OWNER_REPO:$T"
     docker push $OWNER_REPO:$T
 done
