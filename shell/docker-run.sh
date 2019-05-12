433a9b59e127f467f87b76293e4df843d76099ac
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2019-05-12 09:24:22 +0200

    Add simple run scripts

---
+++ shell/docker-run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -x
+
+docker run \
+    --detach \
+    --rm \
+    --name typo3 \
+    --hostname dev.typo3.local \
+    --volume typo3-vol:/var/www/localhost \
+    --publish 127.0.0.1:8080:80 \
+    undecaf/typo3-dev
