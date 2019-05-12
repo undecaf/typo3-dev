c065ca0104a229039f8581d56383aaf2b8379c07
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2019-05-12 14:33:59 +0200

    Add missing HOST_IP

--- shell/podman-run.sh
+++ shell/podman-run.sh
@@ -4,6 +4,7 @@ podman run \
     --detach \
     --name typo3 \
     --hostname dev.typo3.local \
+    --env HOST_IP=$(hostname -I | awk '{print $1}')
     --volume typo3-vol:/var/www/localhost \
     --publish 127.0.0.1:8080:80 \
     undecaf/typo3-dev
