235a5b2b37a3789b188d7bd29a3fd98eb8425517
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2019-06-25 14:28:57 +0200

    Testing Travis testing

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo '********************* Testing'
-docker image ls
+
+set -x
+
+t3 run --
+docker container ls -f name='^/typo3$'
+docker volume ls -f name=typo3-root
+
+t3 stop --
 
 # TODO
