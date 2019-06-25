94cf4a116574a31de2bbc932b11e0171a5eaed0d
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2019-06-25 23:28:23 +0200

    Fix tag in Travis testing

--- .travis/test.sh
+++ .travis/test.sh
@@ -2,12 +2,14 @@
 
 echo '********************* Testing'
 
+# Extract MAJOR.MINOR of $TRAVIS_TAG
+RE='^([0-9]+\.[0-9]+)(\..+)?'
+[[ "$TRAVIS_TAG" =~ $RE ]] && TAG=$TYPO3_VER-${BASH_REMATCH[1]} || TAG=${TYPO3_VER}-latest
+
 set -x -e
 
-./t3 run -d mariadb -t ${TYPO3_VER}-${TRAVIS_TAG:-latest}
+./t3 run -d mariadb -t $TAG
 trap './t3 stop --rm' EXIT
 
 docker container ls -f name='^/typo3(-db)?$'
 docker volume ls -f name='^typo3-(root|data)$'
-
-# TODO
