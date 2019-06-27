d96b296bfce5fe2e4dee97f3edb0304c3fafc402
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2019-06-27 22:06:12 +0200

    Do not push README.md to Dockerhub (too long)

--- .travis/push.sh
+++ .travis/push.sh
@@ -4,7 +4,9 @@ for T in $(.travis/tags.sh); do
     echo '*************** '"Pushing $TRAVIS_REPO_SLUG:$T"
     docker push $TRAVIS_REPO_SLUG:$T
 done
+exit
 
+# README.md exceeds the capabilities of Dockerhub, has to be updated manually
 echo '*************** Pushing README.md'
 docker run --rm \
     -v $(readlink -f README.md):/data/README.md \
