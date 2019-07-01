461d8d71d43267676d6c85b8fbc75f775d1bc049
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2019-07-01 21:43:46 +0200

    Do not cache container image in Travis

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-export IMAGE_VER=${TRAVIS_TAG:-latest}
+if [ "$TRAVIS_BRANCH" = master ]; then
+    export IMAGE_VER=latest
+else
+    export IMAGE_VER=$TRAVIS_BRANCH
+fi
 
-echo '*************** '"TRAVIS_COMMIT: '$TRAVIS_COMMIT'"
 echo '*************** '"TYPO3_VER: '$TYPO3_VER'"
+echo '*************** '"TRAVIS_BRANCH: '$TRAVIS_BRANCH'"
+echo '*************** '"TRAVIS_COMMIT: '$TRAVIS_COMMIT'"
 echo '*************** '"TRAVIS_TAG: '$TRAVIS_TAG'"
 echo '*************** '"IMAGE_VER: '$IMAGE_VER'"
 
 set -x
 
 docker build \
-    --pull \
-    --cache-from $TRAVIS_REPO_SLUG \
     --build-arg COMMIT=$TRAVIS_COMMIT \
     --build-arg TYPO3_VER=$TYPO3_VER \
     --build-arg IMAGE_VER=$IMAGE_VER \
@@ -19,6 +22,9 @@ docker build \
     .
 
 set +x
+
+#    --pull \
+#    --cache-from $TRAVIS_REPO_SLUG \
 
 for T in $(.travis/tags.sh); do 
     echo '*************** '"Tagging $TRAVIS_REPO_SLUG as $TRAVIS_REPO_SLUG:$T"
